ed16d159fc0125b7e7d1bb8fc7b26060df6a074f
FuSE150 <130648328+FuSE150@users.noreply.github.com>
2025-01-07 23:16:57 +0500

    Update server.py

--- crypto1/server.py
+++ crypto1/server.py
@@ -1,1 +1,26 @@
+import socket
 
+def start_server():
+
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(('localhost', 22334))
+    server_socket.listen(1)
+    print("Сервер запущен. Ожидание подключения клиента...")
+
+    conn, addr = server_socket.accept()
+    print(f"Клиент подключен: {addr}")
+
+    while True:
+
+        data = conn.recv(1024)
+        if not data:
+            break
+        print(f"Сообщение от клиента: {data.decode()}")
+
+        message = input("Ответьте клиенту: ")
+        conn.sendall(message.encode())
+
+    conn.close()
+
+if __name__ == "__main__":
+    start_server()
